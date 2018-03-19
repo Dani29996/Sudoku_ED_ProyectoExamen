60f10e19ca1be30ccd2a7a1f64f4d2d66890b384
Daniel Muñoz <daniloja17@gmail.com>
2018-03-19 18:59:42 +0100

    2*Creacion de clase sudoku -Se crea la clase sudoku, la cual incluye. -Cosntructor por defecto -Metodo iniziliazar -Metodo toString

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -5,17 +5,49 @@
  */
 package sudoku;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Dani
  */
-public class Sudoku {
+public class Sudoku 
+{
+    
+   //Variables privadas de la clase
+    private ArrayList<ArrayList<Integer>> sudoku;//En este array de array se guardaran los números del sudoku
 
     /**
-     * @param args the command line arguments
+     * Constructor por defecto
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public Sudoku() 
+    {
+        sudoku = new ArrayList<>();//Inicializamos el array
     }
+     /**
+      * Este metodo inicializa el sudoku
+      */
+    public void inizializar() 
+    {
+        //creamos nueve filas 
+        for (int i = 0; i < 9; i++) 
+        {
+            ArrayList<Integer> fila = new ArrayList<>();//Creamos un array para guardar las filas
+            sudoku.add(fila);//añadimos las filas al array de sudoku
+
+        }
+    }
+    /**
+     * Formatea el sudoku a la hora de mostrarlo por pantalla
+     * @return 
+     */
+    @Override
+    public String toString ()
+    {
+        String resultadoFinal = "";
+        return resultadoFinal;
+    }
+    
+    
     
 }
